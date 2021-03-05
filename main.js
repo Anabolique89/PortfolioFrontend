797ba518e4dc4ba37ea7839c56c30583398cd8f2
NinaAndreeaPopovici <ninandreeapopovici@gmail.com>
2021-03-05 18:54:54 +0100

    scroll up button

--- main.js
+++ main.js
@@ -24,3 +24,24 @@ function complete() {
   clearInterval(timer);
   timer = null;
 }
+
+//smooth scrolling
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 20) {
+      $("#toTopBtn").fadeIn();
+    } else {
+      $("#toTopBtn").fadeOut();
+    }
+  });
+
+  $("#toTopBtn").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+    return false;
+  });
+});
